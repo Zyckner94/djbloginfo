eca45e0b125d494703aa4f9997cbdc2436b79c37
Martin <martin_e.a@hotmail.com>
2022-08-30 15:24:39 -0300

    agregado base de datos para heroku

--- djblog/settings/development.py
+++ djblog/settings/development.py
@@ -1,4 +1,6 @@
 from .base import *
+import dj_database_url
 
 
 ALLOWED_HOSTS = ['*.herokuapp.com', 'djbloginfo.herokuapp.com']
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
