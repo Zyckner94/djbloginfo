2261cda2148bb025625e14278bfdb9f745ea548d
Martin <martin_e.a@hotmail.com>
2022-08-31 10:39:58 -0300

    allowe host

--- djblog/settings/development.py
+++ djblog/settings/development.py
@@ -4,5 +4,5 @@ from .base import *
 
 
 
-ALLOWED_HOSTS = ['*.herokuapp.com', 'djbloginfo.herokuapp.com']
+ALLOWED_HOSTS = ['*.herokuapp.com', 'blog-inf.herokuapp.com']
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
