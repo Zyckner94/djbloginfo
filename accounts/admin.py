880e83afe211644db52e6d74d43860a428943531
Martin <martin_e.a@hotmail.com>
2022-08-31 10:31:19 -0300

    primer commit

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,10 +2,25 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-# class CustomUserAdmin(UserAdmin):
-#     list_display = ('username', 'first_name', 'last_name', 'dni', 'email','ROLE_CHOICES',)
-#     fieldsets:
 
 
 
-admin.site.register(User, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'first_name', 'last_name')
+    fieldsets = (
+        *UserAdmin.fieldsets,  # campos originales del usuario
+        (                      # Agregarmos nuestros campos extra
+            'Información adicional',  # header, puede ser None si no se quiere agregar un header
+            {
+                'fields': (
+                    'dni',
+                    'sexo',
+                    'domicilio',
+                    'telefono',
+                    'role',
+                ),
+            },
+        ),
+    )
+
+admin.site.register(User, CustomUserAdmin)
