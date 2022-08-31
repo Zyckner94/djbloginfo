584838bddf6b2ed468d10127671748153b65e42f
Martin <martin_e.a@hotmail.com>
2022-08-31 11:35:54 -0300

    arreglado allowed hosts

--- djblog/settings/development.py
+++ djblog/settings/development.py
@@ -4,5 +4,5 @@ from .base import *
 
 
 
-ALLOWED_HOSTS = ['*.herokuapp.com', 'blog-inf.herokuapp.com']
+ALLOWED_HOSTS = ['*.herokuapp.com', 'blog-infom.herokuapp.com']
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
